8ebe5a4f9d370c39af620526ab0879ef342fd6b5
Alexei7ca <97787086+Alexei7ca@users.noreply.github.com>
2022-06-23 00:32:02 +0200

    fixed nullPointerException

--- src/main/java/com/mastery/java/task/dto/Employee.java
+++ src/main/java/com/mastery/java/task/dto/Employee.java
@@ -32,10 +32,10 @@ public class Employee {
     }
 
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -55,9 +55,5 @@ public class Employee {
     }
 
 
-    @Override
-    public String toString() {
-        return "Employee [id=" + id + ", firstName=" + firstName + ", gender=" + gender + "]";
-    }
 
 }
